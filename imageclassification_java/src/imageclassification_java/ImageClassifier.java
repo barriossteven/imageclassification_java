fe179e964a6a207f31391ae63fe2be4e998acf1d
barriossteven <barriossteven@gmail.com>
2016-12-14 19:49:49 -0500

    matches train labels with train images (naivebayes)

--- imageclassification_java/src/imageclassification_java/ImageClassifier.java
+++ imageclassification_java/src/imageclassification_java/ImageClassifier.java
@@ -8,14 +8,30 @@ public class ImageClassifier {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		//take test images and gather their features
-		//each row is 28 chars in length
-		//each digit is about 28x28(includes right padding)
-		//some are 14 instead of 20
+		// take test images and gather their features
+		// each row is 28 chars in length
+		// each digit is about 28x28(includes right padding)
+		// some are 14 instead of 20
 		NaiveBayes n = new NaiveBayes();
-		n.getPrior();
-		
+		// n.getPrior();
+		 n.getFeatures();
+
+//		FileReader fr = new FileReader("digitdata/trainingimages.txt");
+//		BufferedReader br = new BufferedReader(fr);
+//		String s;
+//
+//		int line = 0;
+//
+//		while ((s = br.readLine()) != null) {
+//			if (line == 16) {
+//				break;
+//			}
+//			System.out.println(s);
+//		}
+//		while ((s = br.readLine()) != null) {
+//
+//			System.out.println(s);
+//		}
 	}
-	
 
 }
--- imageclassification_java/src/imageclassification_java/NaiveBayes.java
+++ imageclassification_java/src/imageclassification_java/NaiveBayes.java
@@ -13,7 +13,22 @@ public class NaiveBayes {
 	int amounts[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	double priorProbs[] = { 0,0,0,0,0,0,0,0,0,0 };
 	int totalLabels = 0;
-
+	//arrays for each class's feature probabilities
+	double featProb[][][] = new double[10][28][28];
+	
+	public NaiveBayes (){
+		//initializes feature array
+		for(int i = 0; i < featProb.length;i++){
+			//System.out.println(i);
+			for(int j = 0; j < featProb[i].length;j++){
+				for(int k = 0;k<featProb[i][j].length;k++ ){
+					featProb[i][j][k] = 0;
+					//System.out.print( j + "," +k + " ");
+				}
+				//System.out.println("");
+			}
+		}
+	}
 	
 
 	public void getPrior() throws IOException {
@@ -21,20 +36,59 @@ public class NaiveBayes {
 		BufferedReader br = new BufferedReader(fr);
 		String s;
 		while ((s = br.readLine()) != null) {
+			 if(s.trim().isEmpty()){
+				 continue;
+			 }
 			int label = Integer.parseInt(s);
 			this.amounts[label] = amounts[label]+1;
 			this.totalLabels++;
 		}
 		for(int i = 0; i<amounts.length;i++){
-			System.out.println("Amount for " + i + " = " + amounts[i]);
-			System.out.println("total = " + totalLabels);
+			//System.out.println("Amount for " + i + " = " + amounts[i]);
+			//System.out.println("total = " + totalLabels);
 			priorProbs[i] = (double)amounts[i]/totalLabels;
-			System.out.println("Prior prob for " + i + " = " + priorProbs[i]);
+			//System.out.println("Prior prob for " + i + " = " + priorProbs[i]);
 		}
+		fr.close();
+		br.close();
 	}
 	
-	public void get() throws IOException {
+	public void getFeatures() throws IOException {
+		FileReader fr1 = new FileReader(trainingLabels);
+		BufferedReader labels = new BufferedReader(fr1);
 		
+		FileReader fr2 = new FileReader(trainingimages);
+		BufferedReader images = new BufferedReader(fr2);
+		
+		int total = 0;
+		int lineNum = 0;
+		String labelNum;
+		String imageLine;
+		
+		while ((labelNum = labels.readLine()) != null) {
+			total++;
+			lineNum = 0;
+			System.out.println(labelNum);
+			while (lineNum <28) {
+				imageLine = images.readLine();
+				if(imageLine == null){
+					break;
+				}
+				lineNum++;
+				System.out.println(imageLine);
+			}
+			
+			
+		}
+		System.out.println(total);
+		
+		
+		
+		
+		fr1.close();
+		fr2.close();
+		labels.close();
+		images.close();
 	}
 	
 
