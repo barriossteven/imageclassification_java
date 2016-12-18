b36cb06a603711c5e80105d55b85c2f84591d9ab
barriossteven <barriossteven@gmail.com>
2016-12-17 21:04:36 -0500

    added showPercents for all 3

--- imageclassification_java/src/imageclassification_java/ImageClassifier.java
+++ imageclassification_java/src/imageclassification_java/ImageClassifier.java
@@ -14,14 +14,14 @@ public class ImageClassifier {
 		// each digit is 28x28
 		// each face is 70x60
 		
-		//showPercentNaives();
+		showPercents();
 		
 		
-		runNBDigits(1);
-		runPerceptron("digit",3,1);
+		//runNBDigits(1);
+		//runPerceptron("digit",3,1);
 		//runNBFace(1);
 		//runPerceptron("face",3,1);
-		runMira("digit",3,1);
+		//runMira("digit",3,1);
 		//runMira("face",3,1);
 
 	}
@@ -64,27 +64,56 @@ public class ImageClassifier {
 		
 	}
 	
-	public static void showPercentNaives() throws IOException{
+	public static void showPercents() throws IOException{
 		double rateFace_nb;
 		double rateDigit_nb;
 		double percent = .1;
-		
+		int iterations = 3;
 		NaiveBayesFace n;
 		NaiveBayes n2;
+		Perceptron p;
+		Mira m;
 		
+		System.out.println("***********************Starting Naive Bayes on Face******************************");
 		for (int i = 0; i < 10; i++) {
-			n = new NaiveBayesFace(percent,0);
+			n = new NaiveBayesFace(percent*(i+1),0);
 			n.run();
-			percent  = percent+.1;
 		}
-		System.out.println("*****************************************************");
+		System.out.println("***********************Starting Naive Bayes on Digits******************************");
 
 		percent = .1;
 		for (int i = 0; i < 10; i++) {
-			n2 = new NaiveBayes(percent,0);
+			n2 = new NaiveBayes(percent*(i+1),0);
 			n2.run();
-			percent  = percent+.1;
 		}
+		System.out.println("***********************Starting Perceptron on Face******************************");
+		percent = .1;
+		for (int i = 0; i < 10; i++) {
+			p = new Perceptron(0,iterations,percent*(i+1));
+		}
+		System.out.println("***********************Starting Perceptron on Digits******************************");
+		percent = .1;
+		for (int i = 0; i < 10; i++) {
+			p = new Perceptron(1,iterations,percent*(i+1));
+		}
+		System.out.println("***********************Starting Mira on Face******************************");
+		percent = .1;
+		for (int i = 0; i < 10; i++) {
+			m = new Mira(0,iterations,percent*(i+1));
+		}
+		System.out.println("***********************Starting Mira on Digits******************************");
+		percent = .1;
+		for (int i = 0; i < 10; i++) {
+			m = new Mira(1,iterations,percent*(i+1));
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 
 		
 	}
